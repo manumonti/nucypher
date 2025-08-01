fe6b33dc5442c8b67a9eed99bccfe708ebf23f82
derekpierre <derek.pierre@gmail.com>
2025-08-01 11:01:56 -0400

    Remove specification of "me" when generating handover transcript since incoming validator is never part of the existing validator set. The "me" is only needed internally for generating the DKG ferveo object, and therefore the actual validator used as me seemingly doesn't matter and can be any index so why not 0. Therefore, don't expose it as part of initiate_handover.

--- nucypher/blockchain/eth/actors.py
+++ nucypher/blockchain/eth/actors.py
@@ -861,7 +861,6 @@ class Operator(BaseActor):
             nodes=validators,
             aggregated_transcript=aggregated_transcript,
             handover_slot_index=handover_slot_index,
-            me=validators[0],  # FIXME
             ritual_id=ritual_id,
             shares=ritual.dkg_size,
             threshold=ritual.threshold,
--- nucypher/crypto/ferveo/dkg.py
+++ nucypher/crypto/ferveo/dkg.py
@@ -131,7 +131,6 @@ def initiate_handover(
     aggregated_transcript: AggregatedTranscript,
     handover_slot_index: int,
     keypair: Keypair,
-    me: Validator,
     ritual_id: int,
     shares: int,
     threshold: int,
@@ -140,7 +139,12 @@ def initiate_handover(
         raise Exception("missing arguments")  # sanity check
 
     dkg = _make_dkg(
-        me=me, ritual_id=ritual_id, shares=shares, threshold=threshold, nodes=nodes
+        # TODO: is fixed 0-index fine here? I don't believe it matters
+        me=nodes[0],
+        ritual_id=ritual_id,
+        shares=shares,
+        threshold=threshold,
+        nodes=nodes,
     )
     handover_transcript = dkg.generate_handover_transcript(
         aggregated_transcript,
